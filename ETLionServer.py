0e4ffdcb4666a2684fc117b80a138202cc1af0cf
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-11-17 12:53:38 -0500

    Test

--- ETLionServer.py
+++ ETLionServer.py
@@ -22,6 +22,7 @@ from Enum import ORDER_DISCOUNT, ORDER_SIZE, INVENTORY, TRADING_FREQUENCY, QUERY
 from ETLionCore import ETLionCore
 from AppUtil import init_app
 
+
 app = init_app()
 
 db.init_app(app)
